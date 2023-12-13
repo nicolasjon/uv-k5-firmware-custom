2ae49794c591603d34051f322acb5392ff5b836a
Nicolas Jon Pike <nicolas.jon.pike@gmail.com>
2023-12-13 20:33:14 +0000

    User M1HOG branch

--- compile-with-docker.sh
+++ compile-with-docker.sh
@@ -2,4 +2,4 @@
 # cd ~/Documents/VSProjects/uv-k5-firmware-custom
 # ./compile-with-docker.sh
 docker build -t uv-k5-firmware:M1HOG .
-docker run --rm -v ${PWD}/compiled-firmware:/app/compiled-firmware uv-k5-firmware /bin/bash -c "cd /app && make && cp firmware* compiled-firmware/"
+docker run --rm -v ${PWD}/compiled-firmware:/app/compiled-firmware uv-k5-firmware:M1HOG /bin/bash -c "cd /app && make && cp firmware* compiled-firmware/"
